96a505d6f81984f4400e73888957e5c0e94d436c
Parth Patel <ppate220@asu.edu>
2024-11-27 22:41:45 -0700

    IPower.java file modified in accordance to recommandation

--- IPower.java
+++ IPower.java
@@ -2,50 +2,56 @@ package solid3;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 public interface IPower {
 	int toPower(int n, int pow);
 }
 
-class PowerSimple implements IPower {
+ class PowerSimple implements IPower {
+	@Override
 	public int toPower(int n, int pow) {
-		if (pow == 0)
+		if (pow == 0) {
 			return 1;
+		}
+
 		int res = 1;
-		for (int i = 0; i < pow; res *= n, i++)
-			;
-		;
-		;
+		// adding brackets
+			for (int i = 0; i < pow; i++) {
+				res *= n;
+			}
 		return res;
 	}
 }
 
-class PowerCached implements IPower {
-	private static Map<Integer, Map<Integer, Integer>> __cache = new HashMap<Integer, Map<Integer, Integer>>();
+class PowerCached  implements IPower {
+	private static final Map<Integer, Map<Integer, Integer>> __cache = new ConcurrentHashMap<>();
 
-	// resolves the inefficiency in AlmostBest
+	@Override
 	public int toPower(int n, int pow) {
-		Map<Integer, Integer> entry = PowerCached.__cache.get(n);
+			Map<Integer, Integer> entry = __cache.get(n);
 
 		if (entry == null) {
-			entry = new HashMap<Integer, Integer>();
+			// qualifier fix using hmap
+			   entry = new HashMap<>();
+			 __cache.put(n, entry);
 		}
 		return toCachedPower(entry, n, pow);
 	}
 
-	// The contract here is we know there is a cache entry so no check required
-	// (DBC)
+
 	private int toCachedPower(Map<Integer, Integer> e, int n, int pow) {
-		Integer res = null;
+		Integer res;
 		if (pow == 0) {
 			res = 1;
 		} else {
 			res = e.get(pow);
-			if (res != null) {
-				return res;
-			} else {
-				res = n * toCachedPower(e, n, pow - 1);
-				e.put(pow, res); // saves each intermediate result in the cache
+			if (res == null) {
+					res = n * toCachedPower(e, n, pow - 1) ;
+
+				// saving result in cache.
+
+				e.put(pow, res);
 			}
 		}
 		return res;
